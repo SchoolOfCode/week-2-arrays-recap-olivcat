8d17629bc39cde9d28da1a85bfaa1f427e296f9c
“olivcat” <184215778+olivcat@users.noreply.github.com>
2024-12-03 19:27:14 +0000

    Task 1 and 2 Arrays completed

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("hello");
+
+//Example
+// const favoriteFoods = ["Cheese", "Chocolate", "Olives", "Anchovies"];
+
+const favouriteSongs = [
+  "Tiny Dancer",
+  "Running up that Hill",
+  "Don't Stop Moving",
+  "Tragedy",
+  "Crazy Horses",
+];
+
+const pyramidScheme = [1, 2, 4, 8, 16, 32, 64];
+
+let options = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+console.log(options[8]); // Had to remove quotes around options[8], because this treats it as a string, not a variable and just prints the text to the console!
+
+// Using bracket notation and the correct index, for each person...
+//👉 Console.log the month in which you were born.
